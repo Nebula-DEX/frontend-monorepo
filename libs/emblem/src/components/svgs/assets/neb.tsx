e19332754cb188a6919de06c9153bffd8fa70939
oldalbus <oldalbus@protonmail.com>
2024-10-31 12:24:23 -0700

    feat(trading): show currency pair for spot markets

--- libs/emblem/src/components/svgs/assets/neb.tsx
+++ libs/emblem/src/components/svgs/assets/neb.tsx
@@ -3,128 +3,51 @@ import { EmblemSvg } from '../emblem-svg';
 
 export const NEB = (props: SVGAttributes<SVGElement>) => {
   return (
-    <EmblemSvg {...props} viewBox="0 0 32 32" className="p-0.5">
-      <title>NEB logo</title>
-      <g id="NEB_Token_Icon" clipPath="url(#clip0_9399_50281)">
-        <g id="Gradient BG">
-          <rect width="32" height="32" fill="white" />
-          <rect width="32" height="32" fill="url(#paint0_radial_9399_50281)" />
-          <rect
-            width="32"
-            height="32"
-            fill="url(#paint1_radial_9399_50281)"
-            fillOpacity="0.2"
-          />
-          <rect width="32" height="32" fill="url(#paint2_radial_9399_50281)" />
-          <rect width="32" height="32" fill="url(#paint3_radial_9399_50281)" />
-        </g>
-        <g id="N" filter="url(#filter0_d_9399_50281)">
-          <path
-            d="M9 24.1138V7H12.2801L19.7199 18.4092V7H23V24.1138H19.7199L12.2801 12.7046V24.1138H9Z"
-            fill="white"
-            shapeRendering="crispEdges"
-          />
-          <path
-            d="M9 24.1138V7H12.2801L19.7199 18.4092V7H23V24.1138H19.7199L12.2801 12.7046V24.1138H9Z"
-            fill="url(#paint4_linear_9399_50281)"
-            shapeRendering="crispEdges"
-          />
-        </g>
-      </g>
+    <EmblemSvg {...props} viewBox="0 0 176 176">
+      <title>Neb Logo</title>
+      <path d="M0 0H176V176H0V0Z" fill="white" />
+      <path d="M0 0H176V176H0V0Z" fill="url(#gradient0)" />
+      <path d="M0 0H176V176H0V0Z" fill="url(#gradient1)" />
+      <path d="M0 0H176V176H0V0Z" fill="url(#gradient2)" />
+      <path
+        // This is the letter N
+        d="M46.08 140V36.32H65.952L111.024 105.44V36.32H130.896V140H111.024L65.952 70.88V140H46.08Z"
+        fill="#101621"
+      />
       <defs>
-        <filter
-          id="filter0_d_9399_50281"
-          x="9"
-          y="6"
-          width="16"
-          height="19.1137"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dx="1" />
-          <feGaussianBlur stdDeviation="0.5" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0.12549 0 0 0 0 0.133333 0 0 0 0 0.152941 0 0 0 0.04 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_9399_50281"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_9399_50281"
-            result="shape"
-          />
-        </filter>
         <radialGradient
-          id="paint0_radial_9399_50281"
+          id="gradient0"
           cx="0"
           cy="0"
           r="1"
           gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(6.45455 -7.25) rotate(96.9694) scale(40.1453 32.5125)"
+          gradientTransform="translate(35.5 -39.875) rotate(96.9694) scale(220.799 178.819)"
         >
           <stop stopColor="white" stopOpacity="0.2" />
           <stop offset="1" stopColor="#B8FF20" />
         </radialGradient>
         <radialGradient
-          id="paint1_radial_9399_50281"
+          id="gradient1"
           cx="0"
           cy="0"
           r="1"
           gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(16 5.5) rotate(90) scale(26.5 35.2727)"
-        >
-          <stop stopColor="white" />
-          <stop offset="1" stopColor="white" stopOpacity="0.2" />
-        </radialGradient>
-        <radialGradient
-          id="paint2_radial_9399_50281"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(31.4167 0.598642) rotate(135.014) scale(44.4191 36.4399)"
+          gradientTransform="translate(172.792 3.29253) rotate(135.014) scale(244.305 200.42)"
         >
           <stop stopColor="#00BFA5" stopOpacity="0.8" />
           <stop offset="1" stopColor="#00BFA5" stopOpacity="0.06" />
         </radialGradient>
         <radialGradient
-          id="paint3_radial_9399_50281"
+          id="gradient2"
           cx="0"
           cy="0"
           r="1"
           gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(10.8182 35) rotate(-48.4127) scale(28.0769 33.2981)"
+          gradientTransform="translate(59.5 192.5) rotate(-48.4127) scale(154.423 183.14)"
         >
           <stop stopColor="#0075FF" stopOpacity="0.8" />
           <stop offset="0.953677" stopColor="#0075FF" stopOpacity="0" />
         </radialGradient>
-        <linearGradient
-          id="paint4_linear_9399_50281"
-          x1="16"
-          y1="7"
-          x2="16"
-          y2="24.1138"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" stopOpacity="0" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-        <clipPath id="clip0_9399_50281">
-          <rect width="32" height="32" fill="white" />
-        </clipPath>
       </defs>
     </EmblemSvg>
   );
--- libs/emblem/src/components/svgs/markets/neb-spot.tsx
+++ libs/emblem/src/components/svgs/markets/neb-spot.tsx
@@ -1,53 +1,13 @@
 import { type SVGAttributes } from 'react';
-import { EmblemSvg } from '../emblem-svg';
+import { USDT } from '../assets/usdt';
+import { NEB } from '../assets/neb';
+import { cn } from '@vegaprotocol/ui-toolkit';
 
 export const NebSpot = (props: SVGAttributes<SVGElement>) => {
   return (
-    <EmblemSvg {...props} viewBox="0 0 176 176">
-      <path d="M0 0H176V176H0V0Z" fill="white" />
-      <path d="M0 0H176V176H0V0Z" fill="url(#gradient0)" />
-      <path d="M0 0H176V176H0V0Z" fill="url(#gradient1)" />
-      <path d="M0 0H176V176H0V0Z" fill="url(#gradient2)" />
-      <path
-        // This is the letter N
-        d="M46.08 140V36.32H65.952L111.024 105.44V36.32H130.896V140H111.024L65.952 70.88V140H46.08Z"
-        fill="#101621"
-      />
-      <defs>
-        <radialGradient
-          id="gradient0"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(35.5 -39.875) rotate(96.9694) scale(220.799 178.819)"
-        >
-          <stop stopColor="white" stopOpacity="0.2" />
-          <stop offset="1" stopColor="#B8FF20" />
-        </radialGradient>
-        <radialGradient
-          id="gradient1"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(172.792 3.29253) rotate(135.014) scale(244.305 200.42)"
-        >
-          <stop stopColor="#00BFA5" stopOpacity="0.8" />
-          <stop offset="1" stopColor="#00BFA5" stopOpacity="0.06" />
-        </radialGradient>
-        <radialGradient
-          id="gradient2"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(59.5 192.5) rotate(-48.4127) scale(154.423 183.14)"
-        >
-          <stop stopColor="#0075FF" stopOpacity="0.8" />
-          <stop offset="0.953677" stopColor="#0075FF" stopOpacity="0" />
-        </radialGradient>
-      </defs>
-    </EmblemSvg>
+    <span className="relative flex">
+      <NEB {...props} />
+      <USDT {...props} className={cn(props.className, '-ml-2')} />
+    </span>
   );
 };
