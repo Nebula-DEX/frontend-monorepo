bc1e693819f626cdd05fd464b556d55db2d193b7
oldalbus <oldalbus@protonmail.com>
2024-11-13 14:13:04 -0800

    chore: mock candles data from service hok

--- apps/trading/components/market-selector/market-selector-item.spec.tsx
+++ apps/trading/components/market-selector/market-selector-item.spec.tsx
@@ -25,6 +25,10 @@ jest.mock('@vegaprotocol/wallet-react', () => ({
   useChainId: jest.fn(() => '1'),
 }));
 
+jest.mock('@vegaprotocol/rest', () => ({
+  useCandleData: () => ({ sparkline: [] }),
+}));
+
 describe('MarketSelectorItem', () => {
   const yesterday = new Date();
   yesterday.setHours(yesterday.getHours() - 20);
